2b98e7a057290f1641fbdd1c5dd7358f76b497e6
cnsheafe <cnsheafe@gmail.com>
2017-08-23 11:04:43 -0500

    small changes

--- code.ts
+++ code.ts
@@ -10,29 +10,54 @@ var matrix = [
 let m = matrix[0].length;
 let n = matrix.length / m;
 
-const firstLetter = 'O';
+const firstLetter = {
+    color: 'O'
+};
 const secondLetter = 'G';
 
 let output = [];
-
 // Turn 'output' into an array of arrays, with the same number
 // of rows as (the input) matrix
 matrix.forEach((row, i) => {
     output[i] = [];
 });
 
-let myPath = new Path();
 
 
-function TraverseMaze(Maze, path) {
+function TraverseMaze(Maze: string, path, nextColor) {
 // TODO fill out 
+    // look around
+    let currentSpot = Object.assign({}, path.currentSpot);
+    if (Maze[currentSpot.pos-1] === nextColor && currentSpot.visited === 0 ) {
+        path.currentSpot.visited++;
+        path.Add({pos: currentSpot.pos-1, color: Maze[currentSpot.pos-1], visited: 0});
+    }
+    else if (Maze[currentSpot.pos-n] === nextColor && currentSpot.visited === 1 ) {
+        path.currentSpot.visited++;
+        path.Add({pos: currentSpot.pos-n, color: Maze[currentSpot.pos-n], visited: 0});
+    }
+
+    else if (Maze[currentSpot.pos+1] === nextColor && currentSpot.visited === 2 ) {
+        path.currentSpot.visited++;
+        path.Add({pos: currentSpot.pos+1, color: Maze[currentSpot.pos+1], visited: 0});
+    }
+
+    else if (Maze[currentSpot.pos+n] === nextColor && currentSpot.visited === 3 ) {
+        path.currentSpot.visited++;
+        path.Add({pos: currentSpot.pos+n, color: Maze[currentSpot.pos+n], visited: 0});
+    }
+    // nextcolor needs to be referenced from a sequence
+    TraverseMaze(Maze, path, nextColor);
+  
+
 }
 
 for (let i = 0; i < n; i++) {
-    let root = {pos: {x:m-1,y:i},  color: matrix[m-1][n-1+i], visited: 0};
+    // ++visited after moving to next step
+    let root = {pos: {x:m-1,y:n-1+i},  color: matrix[m-1][n-1+i], visited: 0};
     let myPath = new Path(root);
     // TODO implement TraverseMaze
-    TraverseMaze(matrix, myPath);
+    TraverseMaze(matrix, myPath,);
     // Break if we have reached the end of the maze i.e. the first row
     if (myPath.currentSpot.pos.y === 0) {
         break;
